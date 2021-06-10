4ce15f1a181bb2e1eda66a59b759236e3d5f689e
gonzalo <you@example.com>
2021-06-10 16:33:10 -0300

    Ejercicios hasta el 5

--- Ejercicio_5.c
+++ Ejercicio_5.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
+
 
 int main(int argc, char *argv[]) {
   int n = atoi(argv[1]);
   int m = atoi(argv[2]);
+
+  if (n < 0 || m < 0) {
+     printf("Solo numeros positivos\n");
+return 1;
+  }
+  
+
+
   srand(time(NULL));
-  printf("%d\n", rand()(n ));
+  int r = rand();
+  r = (r % (m - n + 1)+ n);
+printf("El numero random es: %d\n", r);
+
+
+
   return 0;
 }
---
+++ Ejercicio_6.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stddio.h>
+#include <time.h>
+
+int main(int argc, char  *argv[]) {
+int a =atoi(argv[1])
+int b =atoi(argv[2]) 
+
+
+
+
+
+
+
+  return 0;
+}
