0dc40bd248355b855511192fd8d551afed0f8bce
gonzalo <you@example.com>
2021-06-10 23:22:12 -0300

    Ejercicios hasta el 13

---
+++ Ejercicio_13.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(void){
+int a = 0;
+int b = 420;
+
+  printf("a=%d\t b=%d\n",a, b );
+srand(time(NULL));
+int r=rand();
+ r=((r%b-a+a)+a);
+ int v=((r%b-a+5)+a);
+if (r==a || r==b || v==a || v==b) {
+((r%b-a+1)+a) && ((v%b-a+5)+a);
+} else {
+  printf("a=%d b=%d\n",r, v);
+}
+  return 0;
+  }
