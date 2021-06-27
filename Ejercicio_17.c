0657426e7b1a214a30748f3f03f493d1abb36af2
gonzalo <you@example.com>
2021-06-26 21:06:10 -0300

    Ejercicios hasta el 17

--- Ejercicio_17.c
+++ Ejercicio_17.c
@@ -1,20 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char  *argv[]) {
   int Days = atoi(argv[1]);
 
-double Year  = Days / 365;
 
-double Week  = (Days/365) / 7;
+printf("Ano: %d\n", Days / 365);
+Days = Days % 365;
 
+printf("Semana: %d\n", Days / 7);
+Days = Days % 7;
 
-
-
-
-
-
-  printf(" Esos segundos equivalen a %f anos y %f semanas \n",Year, Week );
+printf("Dia: %d\n", Days / 1);
+Days = Days % 1;
 
   return 0;
 }
